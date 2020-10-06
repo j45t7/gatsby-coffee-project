c59edce738df5994d077d64f7a5f9060eff084ef
Justyna Marciniak <justy.marciniak@gmail.com>
2020-10-07 08:04:40 +0900

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { FaGulp } from "react-icons/fa"
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <FaGulp />
+  </Layout>
+)
+
+export default IndexPage
